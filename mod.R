1dbc6763470469de99cbc4682f480686e11eee47
keyinvoker <joeriochandra@gmail.com>
2022-07-19 16:41:26 +0800

    added R Markdown

--- mod.R
+++ mod.R
@@ -5,7 +5,7 @@ library(lubridate)
 library(dplyr)
 library(tidyr)
 library(DT)
-
+library(factoextra)
 
 
 # DATA READING
@@ -15,7 +15,6 @@ may_data <- read.csv("dataset/uber-raw-data-may14.csv")
 jun_data <- read.csv("dataset/uber-raw-data-jun14.csv")
 
 uber_data <- rbind(apr_data, may_data, jun_data)
-
 
 
 # PREPROCESSING
@@ -33,7 +32,6 @@ uber_data$dayofweek <- factor(wday(uber_data$Date.Time, label = TRUE))
 uber_data$hour <- factor(hour(hms(uber_data$Time)))
 uber_data$minute <- factor(minute(hms(uber_data$Time)))
 uber_data$second <- factor(second(hms(uber_data$Time)))
-
 
 
 # PLOTTING: trips by hours in a day
@@ -55,7 +53,6 @@ ggplot(month_hour, aes(hour, Total, fill = month)) +
   geom_bar(stat = "identity") +
   ggtitle("Trips by Hour and Month") +
   scale_y_continuous(labels = comma)
-
 
 
 # PLOTTING: trips during every day of the month
@@ -106,8 +103,7 @@ ggplot(month_weekday, aes(month, Total, fill = dayofweek)) +
   scale_fill_manual(values = colors)
 
 
-
-# Finding out the number of Trips by Base
+# CALCULATING NUMBER OF TRIPS USING BASES
 ggplot(uber_data, aes(Base)) + 
   geom_bar(fill = "darkred") +
   scale_y_continuous(labels = comma) +
@@ -116,18 +112,10 @@ ggplot(uber_data, aes(Base)) +
 ggplot(uber_data, aes(Base, fill = month)) + 
   geom_bar(position = "dodge") +
   scale_y_continuous(labels = comma) +
-  ggtitle("Trips by Bases and Month") +
+  ggtitle("Trips by Month and Bases") +
   scale_fill_manual(values = colors)
 
-ggplot(uber_data, aes(Base, fill = dayofweek)) + 
-  geom_bar(position = "dodge") +
-  scale_y_continuous(labels = comma) +
-  ggtitle("Trips by Bases and Day of Week") +
-  scale_fill_manual(values = colors)
-
-
-
-# Creating a Heatmap visualization of day, hour and month)
+# HEATMAP
 month_base <-  uber_data %>%
   group_by(Base, month) %>%
   dplyr::summarize(Total = n())
@@ -136,7 +124,20 @@ ggplot(month_base, aes(Base, month, fill = Total)) +
   geom_tile(color = "white") +
   ggtitle("Heatmap by Month and Bases")
 
-# K-MEANS CLUSTERING
+# MAP - NYC based on Uber Ride Orders
+min_lat <- 40.5774
+max_lat <- 40.9176
+min_long <- -74.15
+max_long <- -73.7004
+
+ggplot(uber_data, aes(x=Lon, y=Lat, color = Base)) +
+  geom_point(size=1) +
+  scale_x_continuous(limits=c(min_long, max_long)) +
+  scale_y_continuous(limits=c(min_lat, max_lat)) +
+  theme_map() +
+  ggtitle("NYC Map Based On Uber Ride Orders During Apr-Jun 2014 by Base")
+
+# K-MEANS CLUSTERING (?)
 # - https://towardsdatascience.com/how-does-uber-use-clustering-43b21e3e6b7d
 # - https://uc-r.github.io/kmeans_clustering
 clus <- uber_data %>%
@@ -146,5 +147,4 @@ clus <- uber_data %>%
 # rmd: let's use k=6 for this case
 k2 <- kmeans(clus, centers = 2, nstart = 25)
 str(k2)
-
-fviz_cluster(k2, data = clus, main = "Clusterfuck")
+fviz_cluster(k2, data = clus, main = "Uber Spread by Cluster with 2 Centers")
